a38c70e183021fbfe8e6806c36a3b20242aa462b
Zeke Nierenberg <hampzan09@gmail.com>
2016-08-16 11:43:17 -0500

    updating hash

--- docker/index.js
+++ docker/index.js
@@ -19,7 +19,7 @@ module.exports = code => {
 
     const stdoutStream = new streamBuffers.WritableStreamBuffer();
 
-    client.run('e3c389a0f137', ["bash", "-c", command], stdoutStream, function(err, data, container) {
+    client.run('2e457f0ac23e', ["bash", "-c", command], stdoutStream, function(err, data, container) {
       if(err) { return reject(err); }
       log('bash:', command);
 
