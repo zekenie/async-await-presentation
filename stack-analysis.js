b1f0639f4bfaf23d0711433ed087ff85096ab4b1
Zeke Nierenberg <hampzan09@gmail.com>
2016-08-17 10:30:27 -0500

    bug

--- stack-analysis.js
+++ stack-analysis.js
@@ -7,7 +7,7 @@ const getFrames = require('./docker');
 app.listen(process.env.PORT || 9001);
 app.use(cors());
 app.use(require('body-parser').json())
-app.post('/', function() {
+app.post('/', function(req, res, next) {
   return getFrames(req.body.code)
     .then(frames => res.json(frames))
     .catch(next);
