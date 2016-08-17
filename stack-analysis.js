4445d9f308e9d1e535144322e81aa2eb6a1f8e8a
Zeke Nierenberg <hampzan09@gmail.com>
2016-08-17 10:36:45 -0500

    unpacking correct thing

--- stack-analysis.js
+++ stack-analysis.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use(require('body-parser').json())
 app.post('/', function(req, res, next) {
   return getFrames(req.body.code)
-    .then(frames => res.json(frames))
+    .then(fileData => {
+      res.set('Content-Type', 'text/json');
+      res.send(fileData.frames);
+    })
     .catch(next);
 });
